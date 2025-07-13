7a1f238643adb59c9c224df2177cc5a85de34970
Alisa <146712422+iggyw1g@users.noreply.github.com>
2025-07-13 19:06:17 +0900

    adding

--- LML/resources/views/voila.blade.php
+++ LML/resources/views/voila.blade.php
@@ -1,5 +1,15 @@
 <!-- resources/views/voila.blade.php -->
+
 <x-app-layout style="overflow: hidden; padding: 0; margin: 0;">
+
+    <button> <a href="" >Linear Regression</a></button>
+
+
+
+    <x-slot name="header" style="overflow: hidden; padding: 0; margin: 0;">
+        <iframe src={{asset("jupyter/Ch03_linreg_lab.html")}} width="100%" height="775"></iframe>
+
+    
     <x-slot name="header" style="overflow: hidden; padding: 0; margin: 0;">
         <iframe src={{asset("jupyter/Ch03_linreg_lab.html")}} width="100%" height="775"></iframe>
     </x-slot>
